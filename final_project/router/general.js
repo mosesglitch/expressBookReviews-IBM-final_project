a8b5e81b58bf31a38e64f19657f86069237338dc
mosesglitch <mosesgachuru15@gmail.com>
2023-04-14 20:59:35 +0300

    add filter by other metrics

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -31,14 +31,18 @@ public_users.get("/author/:author", function (req, res) {
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const title = req.params.title;
+  const filteredObj = Object.fromEntries(
+    Object.entries(books).filter(([key, value]) => value.title === title)
+  );
+  res.send(filteredObj);
 });
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  const bookByIsbn = books[isbn];
+  res.send(bookByIsbn);
 });
 
 module.exports.general = public_users;
